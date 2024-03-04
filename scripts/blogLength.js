8d0be72e8a4353f7877a7c236da28b2ace421dda
Neel <neelnarayanshetty@gmail.com>
2024-03-04 17:26:21 +0530

    Updates

--- scripts/blogLength.js
+++ scripts/blogLength.js
@@ -1,5 +1,6 @@
 function blogLength(id) {
   // split on whitespace and newline
   words = id.innerText.split(/\s+/);
-  document.getElementById("time").innerHTML = words.length / 200 + " mins read";
+  document.getElementById("time").innerHTML =
+    Math.ceil(words.length / 200) + " mins read";
 }
