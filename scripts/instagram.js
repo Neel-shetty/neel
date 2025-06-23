a44d8dacd5f38dc21b7c606f755b694670db1f6b
Neel Narayan Shetty <neelnarayanshetty@gmail.com>
2025-06-23 13:37:14 +0530

    Updates

--- scripts/instagram.js
+++
@@ -1,5 +0,0 @@
-// import data from "../instagram_posts.json" assert { type: "json" };
-// console.log(data);
-fetch("../instagram_posts.json")
-  .then((response) => response.json())
-  .then((json) => console.log(json));
